85e54b85ee2d05c2051b380ee22c617f030dbcbd
Anushka Gattani <anushkagattani1@gmail.com>
2025-09-13 14:29:50 +0530

    Initial Commit

---
+++ klook/klookApp.swift
@@ -0,0 +1,17 @@
+//
+//  klookApp.swift
+//  klook
+//
+//  Created by Anushka Gattani on 13/09/25.
+//
+
+import SwiftUI
+
+@main
+struct klookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
